afa0da0c5c441b2d62b63f8601fe047526be1489
unknown <374688995@qq.com>
2023-03-03 21:39:16 +0800

    聚米电脑

--- testDemo/demo.js
+++ testDemo/demo.js
@@ -1,61 +1,30 @@
-/**
- * 创建透明墙体材质
- * option =>
- * params height color opacity speed
- * **/
-const createOpacityWallMat = ({
-  height = 10,
-  color = "#00ffff",
-  opacity = 0.5,
-  speed = 1,
-}) => {
-  // 顶点着色器
-  const vertexShader = /* glsl */ `
-      uniform vec3 u_color;
+var arr = [1, 2];
+console.log(arr.length);
+arr.length = 3;
+console.log(Object.getOwnPropertyDescriptors(arr));
+/* Object.defineProperty(arr, 'length',{
+  get: function () {
+    console.log('lengthget');
+    return 3
+  },
+  set: function () {
+    console.log('setlength');
+    return 4
+  }
+}) */
 
-      uniform float time;
-      uniform float u_height;
-      varying float v_opacity;
-
-      void main() {
-          vec3 vPosition = position;
-          v_opacity = mix(1.0, 0.0, position.y / u_height * 1.0) * (1.0 + sin(time) * 0.5);
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(vPosition, 1);
-      }
-   `;
-  // 片元着色器
-  const fragmentShader = /* glsl */ `
-      uniform vec3 u_color;
-      uniform float u_opacity;
-      varying float v_opacity;
-      void main() {
-          gl_FragColor = vec4(u_color, v_opacity * u_opacity);
-      }
-    `;
-
-  return new THREE.ShaderMaterial({
-    uniforms: {
-      u_height: {
-        value: height,
-      },
-      u_opacity: {
-        value: opacity,
-      },
-      u_color: {
-        value: new THREE.Color(color),
-      },
-      time: {
-        value: 0,
-      },
-      speed: {
-        value: speed,
-      },
-    },
-    transparent: true,
-    depthWrite: false,
-    depthTest: false,
-    side: THREE.DoubleSide,
-    vertexShader: vertexShader,
-    fragmentShader: fragmentShader,
-  });
-};
+const obj = {
+  name:'zhangsan'
+}
+Object.defineProperty(obj, 'name', {
+  /* configurable指的不是不能修改属性值，是指的修饰符writable enumerable； 当设置configurable为false后，当显示的声明writable为false(默认值)后就不能修改为true，但先指定writable为true还是可以改成默认值(false);感觉configurable描述符作用不大*/
+  configurable: false,
+});
+Object.defineProperty(obj, 'name', {
+  writable: false,
+})
+obj.name = 'lisi'
+Object.defineProperty(obj, 'name', {
+  writable: true
+})
+console.log(obj.name); // lisi
--- testDemo/test.js
+++ testDemo/test.js
@@ -1,7 +1,4 @@
 const bigBox = document.getElementById("bigBox");
 const box = document.getElementById("box");
 
-const fun = (option = { age: 10 }) => {
-  console.log(option.age);
-};
-fun();
+
