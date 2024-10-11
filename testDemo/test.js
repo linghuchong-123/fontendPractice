3d625e37fffa2e221c62014fb2606cf400d1aed2
374688995@qq.com <374688995@qq.com>
2024-10-11 10:06:25 +0800

    司空电脑

--- testDemo/test.js
+++ testDemo/test.js
@@ -1,4 +1,11 @@
-const bigBox = document.getElementById("bigBox");
-const box = document.getElementById("box");
-
-
+const arr = [1, 2, 3, 4, 5];
+let count = 0;
+arr.forEach((value, index) => {
+  count++;
+  if (value == 1) {
+    arr.splice(index, 2);
+    arr.push(index + 10);
+  }
+});
+console.log(arr);
+console.log("循环次数", count);
