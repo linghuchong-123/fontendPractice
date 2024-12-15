98419bcd01c4d468b7aabecf1d9aa0a08baed2cd
374688995 <374688995@qq.com>
2024-12-15 22:11:54 +0800

    验证文件上传功能

--- testDemo/test.js
+++ testDemo/test.js
@@ -1,12 +1,9 @@
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("成功的data数据");
-  }, 100);
-});
-/* 说明.then的优先级高于await，使用then的情况下就不能通过await拿到值了 */
-(async () => {
-  const successData = await promise.then((data) => {
-    console.log("resolve数据", data); // 这里能够进入
-  });
-  console.log(successData); // undefined
-})();
+let hiBlob = new Blob([`<h1>Hi yangsen!<h1>`], { type: "text/html" });
+const blobUrl = window.URL.createObjectURL(hiBlob);
+
+const a = document.createElement("a");
+a.href = blobUrl;
+a.download = "download.txt";
+
+a.click();
+URL.revokeObjectURL(blobUrl);
